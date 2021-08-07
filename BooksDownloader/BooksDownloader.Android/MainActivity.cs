f7eed99d6026c644479ee928903cde00ae373b55
Akira96kill <niko.maiorana@gmail.com>
2021-08-07 19:58:32 +0200

    first message

---
+++ BooksDownloader/BooksDownloader.Android/MainActivity.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android;
+using AndroidX.Core.App;
+using Android.Util;
+using Android.Nfc;
+using Google.Android.Material.Snackbar;
+using AndroidX.Core.Content;
+using System.Threading.Tasks;
+using Android.Widget;
+
+namespace BooksDownloader.Droid
+{
+    [Activity(Label = "BooksDownloader", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            await TryToGetPermissions();
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+        }
+
+
+        async Task TryToGetPermissions()
+        {
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                await GetPermissionsAsync();
+                return;
+            }
+
+
+        }
+        const int RequestLocationId = 0;
+
+        readonly string[] PermissionsGroupLocation =
+            {
+                            //TODO add more permissions
+                            Manifest.Permission.ReadExternalStorage,
+                            Manifest.Permission.WriteExternalStorage,
+             };
+        async Task GetPermissionsAsync()
+        {
+            const string permission = Manifest.Permission.WriteExternalStorage;
+
+            if (CheckSelfPermission(permission) == (int)Android.Content.PM.Permission.Granted)
+            {
+                //TODO change the message to show the permissions name
+                Toast.MakeText(this, "Special permissions granted", ToastLength.Short).Show();
+                return;
+            }
+
+            if (ShouldShowRequestPermissionRationale(permission))
+            {
+                //set alert for executing the task
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Permissions Needed");
+                alert.SetMessage("The application need special permissions to continue");
+                alert.SetPositiveButton("Request Permissions", (senderAlert, args) =>
+                {
+                    RequestPermissions(PermissionsGroupLocation, RequestLocationId);
+                });
+
+                alert.SetNegativeButton("Cancel", (senderAlert, args) =>
+                {
+                    Toast.MakeText(this, "Cancelled!", ToastLength.Short).Show();
+                });
+
+                Dialog dialog = alert.Create();
+                dialog.Show();
+
+
+                return;
+            }
+
+            RequestPermissions(PermissionsGroupLocation, RequestLocationId);
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            switch (requestCode)
+            {
+                case RequestLocationId:
+                    {
+                        if (grantResults[0] == (int)Android.Content.PM.Permission.Granted)
+                        {
+                            Toast.MakeText(this, "Special permissions granted", ToastLength.Short).Show();
+
+                        }
+                        else
+                        {
+                            //Permission Denied :(
+                            Toast.MakeText(this, "Special permissions denied", ToastLength.Short).Show();
+
+                        }
+                    }
+                    break;
+            }
+            //base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ BooksDownloader/BooksDownloader/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Android.Content;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using DownloadManager;
+using Android.OS;
+using Android.Content.PM;
+using Android;
+using NuGet;
+using Xamarin.Essentials;
+
+namespace BooksDownloader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    private void Bottone_Pressed(object sender, EventArgs e)
+        {
+            var autorelibro = autore.Text;
+            var titololibro = titolo.Text;
+            var libroautore = titololibro + "-" + autorelibro;
+            var link = $"https://dwnlg.link/book-n/{autorelibro}/{libroautore}/{titololibro}.pdf";
+            getFile(link, titololibro,autorelibro);
+
+        }
+
+        //non capisco
+        public void getFile(string link, string titololibro, string autorelibro)
+        {
+            //dichiara la directory
+            string dir = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, Android.OS.Environment.DirectoryDownloads);
+
+            try
+            {
+                //dio can poco fa compilava
+                Console.WriteLine(link);
+                Console.WriteLine(dir);
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(link, $"{dir}/{titololibro}.pdf");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
